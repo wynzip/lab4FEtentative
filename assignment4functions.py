2c04dcf63c528b5b14e0e6d371053bd80e680d13
wynzip <francesco.panichi01@fastwebnet.it>
2024-03-19 13:26:25 +0100

    exercise 0 (from davide)

---
+++ assignment4functions.py
@@ -0,0 +1,79 @@
+import pandas as pd
+import numpy as np
+from scipy.stats import t
+
+
+def AnalyticalNormalMeasures(alpha, weights, portfolioValue, riskMeasureTimeIntervalInDay, returns):
+    """
+    This function determines VaR and ES of a portfolio using the parametric method
+    mean/covariance via t-student with nu=4.
+
+    :param alpha:                           significance value
+    :param weights:                         portfolio weights
+    :param portfolioValue:                  notional
+    :param riskMeasureTimeIntervalInDay:    estimation interval in day
+    :param returns:                         returns' matrix
+
+    :return: VaR:                           Value at Risk
+    :return: ES:                            Expected Shortfall
+    """
+
+    # find estimation first date
+    end_date = returns.iloc[-1, 0]  # final date
+    end_date = pd.to_datetime(end_date)  # convert it to datetime format
+    start_date = end_date - pd.Timedelta(days=riskMeasureTimeIntervalInDay)  # to go back 5 years in time
+    start_date = start_date.strftime('%Y-%m-%d')
+
+    # reduced dataset at the estimation interval
+    returns = returns[(returns['Date'] >= start_date)]
+    returns = returns.reset_index(drop=True)
+
+    # substitute NaN with previous data (NaN = missing share price)
+    returns = returns.ffill()
+
+    # compute the log_returns matrix
+    returns.iloc[:, 1:] = returns.iloc[:, 1:].apply(pd.to_numeric, errors='coerce')
+    log_returns = np.log(returns.iloc[:, 1:] / returns.iloc[:, 1:].shift(1))
+    #log_returns2 = np.log(returns[:, 1:].diff())
+    returns.iloc[:, 1:] = log_returns
+    log_returns = returns.drop(returns.index[0])
+
+    # log returns' mean for each analyzed company
+    mu_vector = log_returns.iloc[:, 1:].mean()
+
+    # log returns' standard deviation for each analyzed company
+    std_vector = log_returns.iloc[:, 1:].std()
+    # var_matrix = log_returns.iloc[:, 1:].var()
+
+    # portfolio mean
+    mu_portfolio = np.dot(weights, mu_vector)
+
+    # portfolio std
+
+    std_portfolio = np.dot(weights, std_vector)
+
+    # degrees of freedom
+    nu = 4
+
+    # standard VaR
+    VaR_std = t.ppf(alpha, df=nu)
+
+    # VaR with variance/covariance method
+    VaR = mu_portfolio + std_portfolio * VaR_std
+
+    # portfolio VaR
+    var_value = portfolioValue * VaR
+
+    # t-student probability density function
+    phi_t = t.pdf(VaR_std, df=nu)
+
+    # standard ES
+    ES_std = ((nu + VaR_std ** 2) / (nu - 1)) * (phi_t / (1 - alpha))
+
+    # ES with variance/covariance method
+    ES = mu_portfolio + std_portfolio * ES_std
+
+    # portfolio ES
+    es_value = portfolioValue * ES
+
+    return var_value, es_value
---
+++ exercise0.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import numpy as np
+from assignment4functions import AnalyticalNormalMeasures
+
+# load EUROSTOXX_Dataset
+file_csv = "EUROSTOXX50_Dataset.csv"
+Dataset = pd.read_csv(file_csv)
+
+# name of columns useful for exercise 0
+columns_name = ["Date", "ADSGn.DE", "ALVG.DE", "MUVGn.DE", "OREP.PA"]
+
+# reduced dataset with companies to analyze
+Dataset_0 = Dataset[columns_name]
+
+# last date
+end_date = '2020-02-20'
+
+# reduced dataset until 20-02-2020
+Dataset_0 = Dataset_0[(Dataset_0['Date'] <= end_date)]
+Dataset_0 = Dataset_0.reset_index(drop=True)
+
+# portfolio weights
+weights = np.full(4, 0.25)
+
+# significance value
+alpha = 0.99
+
+# value of portfolio (€)
+portfolioValue = 1.5e7  # 15000000
+
+# estimation interval
+years = 5
+days_for_year = 365
+riskMeasureTimeIntervalInDay = years*days_for_year+1
+
+VaR_portfolio, ES_portfolio = AnalyticalNormalMeasures(alpha, weights, portfolioValue, riskMeasureTimeIntervalInDay,
+                                                       Dataset_0)
+
+print(VaR_portfolio)
+print(ES_portfolio)
+
+
+
+
